d03e7fd997c14b664f8c772ce933dbf373f5f671
JaimyVA <jaimy2805ftw@gmail.com>
2022-01-11 13:31:27 +0100

    Added IdentityOptions

--- MovieStoreExamen/Program.cs
+++ MovieStoreExamen/Program.cs
@@ -19,6 +19,28 @@ builder.Services.AddDefaultIdentity<MovieStoreExamenUser>(options => options.Sig
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Wijzig de standaard settings van Identity als nodig
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
